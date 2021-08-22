39824fe01e63b870b7996b86deab004e158f428e
kaikaigame <kairemake@outlook.com>
2021-08-22 21:22:56 +0900

    2021.1.17

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -22,7 +23,11 @@ public class GameManager : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
     }
 
     public void GoToMainMenu()
--- README.md
+++ README.md
@@ -12,10 +12,9 @@ https://assetstore.unity.com/packages/essentials/asset-packs/2d-beginner-tutoria
   
 https://assetstore.unity.com/packages/essentials/tutorial-projects/2d-beginner-complete-project-140253  
   
-Unity m_EditorVersion:  2021.1.12f1  
+Unity m_EditorVersion:  2021.1.17f1  
   
 KEYBOARD  
-  
 UP: w or up  
 DOWN: s or down  
 LEFT: a or left  
